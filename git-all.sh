509139ecfc6550e8c14189a4f6d70ed5566517d7
Ethelnol <caitlynhbriggs@gmail.com>
2025-10-22 09:59:49 -0700

    Added build-only option
    Remove old commented lines

--- git-all.sh
+++ git-all.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+declare build=1
 declare forcebuild=0
 declare quiet=0
 declare file=".source.txt"
 
 for i in "$@"; do
 	[[ "$i" == "--force-build" ]] && forcebuild=1 && file=".build.txt" && continue
+	[[ "$i" == "--only-update" || "$i" == "--no-build" ]] && build=0 && continue
 	[[ "$i" == "-q" || "$i" == "--quiet" ]] && quiet=1 && continue
 	[[ "$i" != "-h" && "$i" != "--help" ]] && 1>&2 echo "unknown option - \"$i\"" && exit 1
 
-	echo    "$(basename "$0"):"
+	echo    "git-all.sh:"
 	echo -e "Automatic update and build script for git\n"
 	echo    "Update:"
 	echo    "  File must be a single line."
@@ -19,22 +21,18 @@ for i in "$@"; do
 	echo    "  Sources \".build.txt\" in project root after a successful update if the file is present."
 	echo -e "  Instructions must be valid shell code.\n"
 	echo    "Options:"
-	echo    "     -h, --help:  Display this message"
-	echo    "  --force-build:  Don't fetch updates and build all repos with .build.txt file"
-	echo    "    -q, --quiet:  Disable terminal output"
-	exit 1
+	echo    "     -h, --help:            Display this message"
+	echo    "--only-update, --no-build:  Don't build after fetching updates"
+	echo    "  --force-build:            Don't fetch updates and build all repos with .build.txt file"
+	echo    "    -q, --quiet:            Disable terminal output"
+	exit 0
 done
 
 #single ping occasionally eroniously errors so run a double ping to be sure
-if ! ping "github.com" -c 1 &>/dev/null && ! ping "github.com" -c 2 &>/dev/null; then
-        echo "Unable to connect to GitHub"
+if ! ping "www.google.com" -c 1 &>/dev/null && ! ping "www.google.com" -c 2 &>/dev/null; then
+        echo "Unable to connect to the internet"
         exit 1
 fi
-
-#if [[ "$(dirname "$0")" != "." ]]; then
-#	1>&2 echo "Must be in the same directory as git-all.sh"
-#	exit 1
-#fi
 
 declare gitDir="$(dirname "$0")"
 declare curDir="$PWD"
@@ -43,7 +41,6 @@ declare end=""
 declare l=0
 declare largestLen=0
 declare project=""
-#declare logtime="$(date '+%Y%m%d')"
 
 for i in "$gitDir/"*/$file; do
 	[[ "$i" == "$gitDir/*/$file" ]] && continue;
@@ -56,7 +53,6 @@ largestLen=$(( largestLen + 1 ))
 
 clean(){
 	declare x=$1
-	#eval "printf -- '\b %.0s' {0..$1}"
 	while (( x > 0 )); do
 		echo -ne "\b \b"
 		x=$(( x - 1 ))
@@ -72,9 +68,12 @@ pull(){
 
 #main process here
 
-(( ! quiet )) && echo -n "  repository:" &&\
-	eval "printf -- ' %.0s' {10..$largestLen}" &&\
+if (( ! quiet )); then
+	echo -n "  repository:"
+	eval "printf -- ' %.0s' {10..$largestLen}"
 	echo -e "status\n"
+fi
+
 for i in "$gitDir"/*/.source.txt; do
 	project="$(dirname "$i")"
         [[ "$i" == "$gitDir/*/.source.txt" || ! -d "$project" ]] && continue
@@ -101,7 +100,7 @@ for i in "$gitDir"/*/.source.txt; do
 		if [[ "$end" == Aborting || "$end" == Updating* ]]; then
 			(( ! quiet )) && echo -n "removing merge files"
 			for k in $(echo "$str" | tail -$(( l - 3 )) | head -$(( l - 6 ))); do
-				[[ -f "$k" ]] && rm "$k"
+				[[ -f "$k" ]] && rm --force "$k"
 			done
 
 			pull
@@ -115,7 +114,7 @@ for i in "$gitDir"/*/.source.txt; do
 		fi
 	fi
 
-	if [[ ! -f .build.txt ]]; then
+	if [[ ! -f .build.txt ]] || (( ! build )); then
 		(( ! quiet )) && echo "update complete"
 		cd "$curDir"
 		continue
@@ -124,8 +123,6 @@ for i in "$gitDir"/*/.source.txt; do
 	(( ! quiet )) && echo -n "building"
 
 	[[ ! -f .build.log ]] && touch .build.log && chmod u+w .build.log
-#	. .build.txt &> ".build.$(date '+%Y%m%d%H%M').log"
-#	. .build.txt &> ".build.$logtime.log"
 	. .build.txt &> .build.log
 	declare failed=$?
 
